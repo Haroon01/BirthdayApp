27803ceed1d14071b6a6b566476c007648051661
haroon01 <haroon815@gmail.com>
2020-04-04 01:10:04 +0100

    finished splash screen

--- app/src/main/java/com/haroon/mybirthday/SplashScreenActivity.java
+++ app/src/main/java/com/haroon/mybirthday/SplashScreenActivity.java
@@ -19,7 +19,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
-    private static int SPLASH_SCREEN = 5000;
+    private static int SPLASH_SCREEN = 7000;
 
     Animation topAnim, bottomAnim;
     ImageView logo;
@@ -28,8 +28,7 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        hideSystemUI();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splashscreen);
 
         //set animations
@@ -54,14 +53,6 @@ public class SplashScreenActivity extends AppCompatActivity {
         },SPLASH_SCREEN);
     }
 
-    public void hideSystemUI() {
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_LOW_PROFILE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-    }
+
 }
 
