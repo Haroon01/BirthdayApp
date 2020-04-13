dcae34f376a0dc5b3e9039b790f17e7f0a60ffaf
haroon01 <haroon815@gmail.com>
2020-04-13 03:05:35 +0100

    finished location screen

--- app/src/main/java/com/haroon/mybirthday/LocationActivity.java
+++ app/src/main/java/com/haroon/mybirthday/LocationActivity.java
@@ -1,13 +1,23 @@
 package com.haroon.mybirthday;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+import java.net.URI;
 import java.util.Objects;
 
 public class LocationActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +25,23 @@ public class LocationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_location);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         setTitle(R.string.btn_location);
+
+        Button btnOpenMaps = findViewById(R.id.btnOpenMaps);
+
+        btnOpenMaps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+
+                String label = "label";
+
+                intent.setData(Uri.parse("geo:53.667137,-1.482669?q=53.667137,-1.482669("+label+")"));
+
+                startActivity(intent);
+            }
+        });
+
     }
+
+
 }
