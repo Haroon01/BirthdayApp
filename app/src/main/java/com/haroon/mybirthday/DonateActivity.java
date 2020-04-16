486d5d183402d458073f15b1bf3cca99aca9f38d
haroon01 <haroon815@gmail.com>
2020-04-16 01:51:12 +0100

    completed donate screen

--- app/src/main/java/com/haroon/mybirthday/DonateActivity.java
+++ app/src/main/java/com/haroon/mybirthday/DonateActivity.java
@@ -3,6 +3,16 @@ package com.haroon.mybirthday;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.strictmode.WebViewMethodCalledOnWrongThreadViolation;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.util.JsonUtils;
+
+import java.util.concurrent.TimeUnit;
 
 import java.util.Objects;
 
@@ -14,6 +24,40 @@ public class DonateActivity extends AppCompatActivity {
         setTheme(R.style.AppTheme_ActionBar);
         setContentView(R.layout.activity_donate);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
-        setTitle(R.string.btn_donate);
+
+        Button btnDonate = findViewById(R.id.btnDonate);
+        final TextView txtName = findViewById(R.id.txtDonateName);
+        final TextView txtAmnt = findViewById(R.id.txtdonateAmt);
+        final TextView txtMsg = findViewById(R.id.txtdonatemsg);
+
+
+        btnDonate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = txtName.getText().toString();
+                String amnt = txtAmnt.getText().toString();
+                String msg = txtMsg.getText().toString();
+
+                if(name.isEmpty() || amnt.isEmpty()){
+                    toast("Please enter a name and amount");
+                }
+                else{
+                    toast("Sending...");
+
+                    txtName.setText("");
+                    txtMsg.setText("");
+                    txtAmnt.setText("");
+
+                    toast("Successful! Thank you!!");
+                }
+
+            }
+        });
+
+
+    }
+
+    private void toast(String text){
+        Toast.makeText(getApplicationContext(),text, Toast.LENGTH_LONG).show();
     }
 }
